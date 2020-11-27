1b0df46cf32a460b2bea9b644d7781e6af572393
omur8819 <omur8819@gmail.com>
2020-11-28 01:28:39 +0300

    Project updated

--- src/App.js
+++ src/App.js
@@ -1,43 +1,54 @@
-import { useState } from 'react';
-import Player from "./components/Player";
+import {useState, useEffect} from 'react';
+import Player from './components/Player/Player';
 
 function App() {
+  const [songs] = useState([
+    {
+      title: "Forget me too ft. Halsey",
+      artist: "Machine Gun Kelly",
+      img_src: "./images/song-1.jpg",
+      src: "./music/on-n-on.mp3"
+    },
+    {
+      title: "Song 2",
+      artist: "Artist 2",
+      img_src: "./images/song-2.jpg",
+      src: "./music/somebody-new.mp3"
+    },
+    {
+      title: "Song 3",
+      artist: "Artist 3",
+      img_src: "./images/song-3.jpg",
+      src: "./music/on-n-on.mp3"
+    },
+    {
+      title: "Song 4",
+      artist: "Artist 4",
+      img_src: "./images/song-4.jpg",
+      src: "./music/somebody-new.mp3"
+    }
+  ]);
 
-const[songs, setSongs] = useState([
-  {
-    title: "Forget me too ft. Halsey",
-    artist: "Machine Gun Kelly",
-    img_src: "./images/song-1.jpg",
-    src: "./music/on-n-on.mp3"
-  },
-  {
-    title: "Song 2",
-    artist: "Artist 2",
-    img_src: "./images/song-2.jpg",
-    src: "./music/somebody-new.mp3"
-  },
-  {
-    title: "Song 3",
-    artist: "Artist 3",
-    img_src: "./images/song-3.jpg",
-    src: "./music/on-n-on.mp3"
-  },
-  {
-    title: "Song 4",
-    artist: "Artist 4",
-    img_src: "./images/song-4.jpg",
-    src: "./music/somebody-new.mp3"
-  }
-]);
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [nextSongIndex, setNextSongIndex] = useState(0);
 
-const[currentSongIndex, setCurrentSongIndex] = useState(0);
-const[nextSongIndex, setNextSongIndex] = useState(currentSongIndex + 1);
+  useEffect(() => {
+    setNextSongIndex(() => {
+      if (currentSongIndex + 1 > songs.length - 1) {
+        return 0;
+      } else {
+        return currentSongIndex + 1;
+      }
+    });
+  }, [currentSongIndex]);
 
   return (
     <div className="App">
       <Player 
-        song={songs[currentSongIndex]}
-        nextSong={songs[nextSongIndex]}  
+        currentSongIndex={currentSongIndex} 
+        setCurrentSongIndex={setCurrentSongIndex} 
+        nextSongIndex={nextSongIndex} 
+        songs={songs}
       />
     </div>
   );
